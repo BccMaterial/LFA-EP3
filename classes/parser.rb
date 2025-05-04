b94f5480efe7f9c6ad57ff2d217c8f18338c7edc
thiagopls1 <thiagopls1@hotmail.com>
2025-05-03 21:54:23 -0300

    feat: add `RecursiveDescentParserJson` class

--- classes/parser.rb
+++ classes/parser.rb
@@ -67,3 +67,74 @@ class RecursiveDescentParserMath
   end
 end
 
+class RecursiveDescentParserJson
+  # Vem primeiro: Primitivo, Lista ou Objeto
+  # Dentro de objeto:
+  #   Vem primeiro: STRING
+  #   Vem segundo: COLON
+  #   Vem terceiro: Primitivo
+  #   Vem quarto: COMMA
+  # Dentro de lista:
+  #   Vem primeiro: Primitivo
+  #   Vem segundo: COMMA
+  #   Vem terceiro: Primitivo
+  #   .
+  #   .
+  #   .
+  #   Vem último: RBRACKET
+   
+  def initialize(tokens)
+    @tokens = tokens
+    @position = 0
+  end
+
+  def parse
+    return expr
+  end
+
+  private
+
+  def current_token
+    return @tokens[@position]
+  end
+
+  def eat(expected_type)
+    if current_token&.type == expected_type
+      token = current_token
+      @position += 1
+      return token
+    else
+      raise "Unexpected token: #{current_token}, expected #{expected_type}"
+    end
+  end
+
+  def list
+  end
+
+  def item
+  end
+
+  def object
+  end
+
+  def field
+  end
+
+  def factor
+    if current_token.type in [:STRING, :BOOLEAN, :NULL, :NUMBER, :MATH]
+      eat(current_token.type).value
+    elsif current_token.type == :LBRACE
+      eat(:LBRACE)
+      result = expr
+      eat(:RBRACE)
+      return result
+    elsif current_token.type == :LBRACKET
+      eat(:LBRACKET)
+      result = expr
+      eat(:RBRACKET)
+      return result
+    else
+      raise "Unexpected token in factor: #{current_token}"
+    end
+  end
+end
